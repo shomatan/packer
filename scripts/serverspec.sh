f6820d9f35dc3eb9fc11406ae57ed2a14b3bdb73
Shoma Nishitateno <shoma416@gmail.com>
2016-08-25 13:14:45 +0900

    Fix - install ruby.

--- scripts/serverspec.sh
+++ scripts/serverspec.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-export PATH=/opt/chef/embedded/bin:$PATH
+#export PATH=/opt/chef/embedded/bin:$PATH
+yum -y install ruby
+
 cd /tmp/tests
 bundle install --path=vendor
 bundle exec rake spec
+
+yum -y remove ruby
