6bcc378e465d5ba2487c4653ae64ee656b49594c
Shoma Nishitateno <shoma416@gmail.com>
2016-09-30 15:11:03 +0900

    Build success.

--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -2,5 +2,6 @@
 echo "--------------------------------------------------------------"
 echo "  Zero out the free space to save space in the final image "
 echo "--------------------------------------------------------------"
+yum -y clean all
 dd if=/dev/zero of=/EMPTY bs=1M > /dev/null
 rm -f /EMPTY
--- scripts/init.sh
+++ scripts/init.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-yum -y install git
-curl -sS https://raw.githubusercontent.com/shomatan/init-scripts/master/init.sh | bash
-curl -sS https://raw.githubusercontent.com/shomatan/init-scripts/master/neovim.sh | bash
-curl -sS https://raw.githubusercontent.com/shomatan/dotfiles/master/tools/install.sh | bash
-
-
-# Install dotfiles
-su - vagrant -c "curl -sS https://raw.githubusercontent.com/shomatan/dotfiles/master/tools/install.sh | bash"
-chsh -s /bin/zsh vagrant
+yum -y update
+yum -y install epel-release
+yum -y install ansible
